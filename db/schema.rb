cfbe8a2cb55b448c04e3a1bdc7cdbda8f79df12a
marcopavan <marcopavan83@gmail.com>
2012-01-02 13:02:44 +0100

    I1: adding users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111230182025) do
+ActiveRecord::Schema.define(:version => 20120102110332) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(:version => 20111230182025) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       :precision => 8, :scale => 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
